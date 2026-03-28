c8fe3358ff967732fb0840cc0c68bbd77859c887
katsugrace <victor.drobizov@gmail.com>
2026-03-28 15:27:19 +0500

    feat(telegram-notifier): Перенесены парамметры в notifier

--- src/notifiers/builder.py
+++ src/notifiers/builder.py
@@ -1,21 +1,15 @@
+from typing import Dict
 from src.notifiers.factory import NotifierFactory
 from src.config.models import NotifierConfig
-from typing import Dict
 
 
 def build_notifiers(
         notifiers_config: Dict[str, NotifierConfig]) -> Dict[str, object]:
     result = {}
     for name, cfg in notifiers_config.items():
-        kwargs = {}
-
-        token = cfg.get_token()
-        if token:
-            kwargs['token'] = token
-
-        if cfg.chat_ids:
-            kwargs['chat_ids'] = cfg.chat_ids
-
-        result[name] = NotifierFactory.create(cfg.type, **kwargs)
+        result[name] = NotifierFactory.create(
+            cfg.type,
+            **cfg.dict(exclude={'type'})
+        )
 
     return result

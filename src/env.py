e19287761f04724d688e216d21144de74bff6b94
katsugrace <victor.drobizov@gmail.com>
2026-03-29 14:35:00 +0500

    test(env): Добавлены тесты загрузки переменных окружения

--- src/env.py
+++ src/env.py
@@ -3,11 +3,12 @@ from pydantic import Field
 
 
 class EnvConfig(BaseSettings):
-    config: str = Field('config/triggers.yaml', env='CONFIG_PATH')
-    host: str = Field('127.0.0.1', env='HOST')
-    port: int = Field(8000, env='PORT')
+    config: str = Field('config/triggers.yaml', validation_alias='CONFIG_PATH')
+    host: str = Field('127.0.0.1', validation_alias='HOST')
+    port: int = Field(8000, validation_alias='PORT')
 
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
-        extra = 'allow'
+    model_config = {
+        "env_file": ".env",
+        "env_file_encoding": "utf-8",
+        "extra": "allow",
+    }

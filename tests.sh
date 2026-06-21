125ca2b9871c7f0f6a4d0f07b7792ca3dad8db52
katsugrace <victor.drobizov@gmail.com>
2026-06-21 22:48:25 +0500

    feat(ci): Добавлен workflow

--- tests.sh
+++ tests.sh
@@ -4,6 +4,14 @@ set -e
 
 mkdir -p assets
 
+if command -v flake8 >/dev/null 2>&1; then
+	flake8 src tests
+else
+	if python3 -m flake8 --version >/dev/null 2>&1; then
+		python3 -m flake8 src tests
+	fi
+fi
+
 python3 -m pytest --cov=src --cov-report=xml
 
 if command -v coverage-badge >/dev/null 2>&1; then

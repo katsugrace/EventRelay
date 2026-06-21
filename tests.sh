8f3a97c153478843e2a6e1255f85cb700944791b
katsugrace <victor.drobizov@gmail.com>
2026-06-21 23:13:10 +0500

    feat: Заменена библеотека генерации badge

--- tests.sh
+++ tests.sh
@@ -14,12 +14,4 @@ fi
 
 python3 -m pytest --cov=src --cov-report=xml
 
-if command -v coverage-badge >/dev/null 2>&1; then
-	coverage-badge -f -o assets/coverage-badge.svg
-else
-	if python3 -m coverage_badge -h >/dev/null 2>&1; then
-		python3 -m coverage_badge -f -o assets/coverage-badge.svg
-	else
-		exit 0
-	fi
-fi
+genbadge coverage -i coverage.xml -o assets/coverage-badge.svg
